4bce041c5a4077f004e1391aebc86ff2597d7a01
Amir Mustafa <amirengg15@gmail.com>
2017-05-15 04:33:27 +0530

    Added Latest India Map

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>India Map(Amir Mustafa)</title>
+</head>
+<body>
+	<br><br>
+	<h1 style="margin-left: 20%">Latest India Map (Hyderabad - Telangana Division)</h1><br>
+	<center>
+		
+		<?php include('india.svg'); ?>
+	</center>
+</body>
+</html>
